3a094ba7ebf6b14ef4f9e4cd9e3cb1929272f699
Quwanish <quwanishmi@gmail.com>
2024-03-01 09:56:06 +0500

    fix

--- src/index.js
+++ src/index.js
@@ -1,26 +1,38 @@
-import React, {useState} from "react";
 import ReactDOM from "react-dom/client";
+import React from "react";
+// import App from "./App";
+import "./index.css"
+import { useState } from 'react';
+ 
+// import React, { useState } from 'react';
 
+const InputClear = () => {
+  const [text, setText] = useState('');
 
-
-
-const Counter = () => {
-  const [count, setCount] = useState(0);
-
-  const handleIncrement = () => {
-    setCount(prevCount => prevCount + 1);
+  const handleClear = () => {
+    setText('');
   };
 
   return (
-    <div>
-      <h1>Counter: {count}</h1>
-      <button onClick={handleIncrement}>Increment</button>
+    
+    <div className="container">
+      <h1>Сброс пароля</h1>
+      <p>Введите адрес электронной почты, 
+        связанный с вашим аккаунтом.</p>
+        
+      <input
+      
+        type="text"
+        placeholder="Введите email"
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      />
+      <button type="button" onClick={handleClear} >Сбросит</button>
     </div>
   );
 };
- 
 
- 
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Counter/>);
+root.render(<InputClear />);
