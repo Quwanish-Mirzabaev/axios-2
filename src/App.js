753ee5c93ae732cf4c9202f7c7dd185912126b04
Quwanish <quwanishmi@gmail.com>
2024-03-13 09:37:01 +0500

    fix

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import React, { useState } from "react";
 import "./App.css";
 
-function App() {
+const ThemeContext = React.createContext();
 
-  const [theme, setTheme] = useState(false)
+function App() {
+  const [theme, setTheme] = useState(false);
 
   return (
-    <div class={theme ? "theme-dark" : ""}>
-      <div class="content-bg-color main-content">
-        <h1>Dark</h1>
-        <input type="checkbox" onChange={() => setTheme(!theme)} />
+    <ThemeContext.Provider value={{ theme, setTheme }}>
+      <div className={theme ? "theme-dark" : ""}>
+        <div className="content-bg-color main-content">
+          <h1>Dark</h1>
+          <input className="inp" type="checkbox" onChange={() => setTheme(!theme)} />
+        </div>
       </div>
-    </div>
+    </ThemeContext.Provider>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,17 +1,56 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { createContext, useContext, useRef, useState } from 'react';
+import ReactDOM from 'react-dom';
+import './index.css'
+// import logo from './logo.svg'
+import logo from './asd (2).png'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const UserContext = createContext();
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function UserLogIn() {
+  const [user, setUser] = useState({});
+  const nameRef = useRef();
+  const emailRef = useRef();
+
+  const handleLogIn = () => {
+    const name = nameRef.current.value;
+    const email = emailRef.current.value;
+    setUser({name, email});
+  }
+
+  
+
+  return (
+    <UserContext.Provider value={user}>
+      <div className='login'>
+        <h2>User Log In</h2>
+        <input className='inp' type="text" placeholder=" your name" ref={nameRef} />
+        <input className='inp2' type="email" placeholder=" your email" ref={emailRef} />
+        <button onClick={handleLogIn}>Log In</button>
+      </div>
+      <UserProfile />
+    </UserContext.Provider>
+  );
+}
+
+function UserProfile() {
+  const user = useContext(UserContext);
+
+  return (
+    <div className='profile'>
+      <h2>User Profile</h2>
+      
+      <p>Name: {user.name}</p>
+      <p>Email: {user.email}</p>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <UserLogIn />
+    </div>
+  );
+}
+const root = document.getElementById("root");
+ReactDOM.render(<App/>, root);
